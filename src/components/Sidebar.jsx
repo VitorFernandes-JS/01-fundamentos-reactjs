1fde8260d2f2ae3216ebc60c802b9589be9b10ce
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-11-30 15:31:46 -0300

    feat: sidebar finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,30 @@
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
-    return(
-        <aside className={styles.sidebar}>
-            <img src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"  />
-        </aside>
-    )
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/Vitor-php.png"
+          alt="Foto de perfil"
+        />
+        <strong>Vitor Fernandes</strong>
+        <span>Web Developer</span>
+
+        <footer>
+          <a href="#">
+            <PencilLine size={22}/>
+            Edição de Perfil
+          </a>
+        </footer>
+      </div>
+    </aside>
+  );
 }
